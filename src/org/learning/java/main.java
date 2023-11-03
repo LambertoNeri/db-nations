791db9f35712de8365fc5bd4b939b9e4d53ba92a
LambertoNeri <neri.lamberto91@gmail.com>
2023-11-03 14:32:54 +0100

    first commit

--- src/org/learning/java/main.java
+++ src/org/learning/java/main.java
@@ -1,4 +1,30 @@
 package org.learning.java;
 
-public class sql {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class main {
+    public static void main(String[] args) {
+        // Parametri di connessione
+        String url = "jdbc:mysql://localhost:3306/db_nations";
+        String user = "root";
+        String password = "root";
+
+        // provo ad aprire una connessione con try-with-resources
+
+        try (Connection connection = DriverManager.getConnection(url, user,password)) {
+            // dentro al try ho la connection aperta
+            // provo un sout per testare la connessione
+            // System.out.println(connection.getCatalog());
+
+
+
+
+        }catch (SQLException e) {
+            System.out.println("Unable to connect");
+            e.printStackTrace();
+        }
+
+    }
 }
